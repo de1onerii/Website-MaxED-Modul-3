5fced65eaf7e9c1a38b1e66401493ec2ea520636
Yaroslav <138430337+de1onerii@users.noreply.github.com>
2023-09-17 10:29:33 +0700

    ДЗ-6, Модуль 3

---
+++ ДЗ-6/index.php
@@ -0,0 +1,13 @@
+<?php
+class User {
+    private $name = ' ';
+
+    public function getName($name) {
+        $this->name = $name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
+?>
